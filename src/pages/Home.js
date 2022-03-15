d3cb633c02310efc34b4062dd2ede569c2703319
OgutuJoseph <onyango.joseph@yahoo.com>
2022-03-15 21:38:27 +0300

    Cards and Card Items

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../App.css';
 import HeroSection from '../components/HeroSection/HeroSection';
+import Cards from '../components/Cards/Cards';
 
 function Home () {
     return (
         <>
             <HeroSection />
+            <Cards />
         </>
     )
 }
