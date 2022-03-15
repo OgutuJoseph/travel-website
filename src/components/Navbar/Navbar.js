e5c83cbcabd9cd7d7212395c37fcc1158da56450
OgutuJoseph <onyango.joseph@yahoo.com>
2022-03-15 09:09:21 +0300

    Hero Section

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from '../Button/Button';
 
 function Navbar() {
 
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960)
+        {
+            setButton(false);
+        }
+        else 
+        {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                         TRVL<i className='fab fa-typo3'></i>
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
@@ -35,11 +55,12 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className='nav-item'> 
-                            <Link to='/sign-up' className='nav-links=mobile' onClick={closeMobileMenu}>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
                                 Sign Up
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
                 </div>
             </nav>
         </>
